556649b9420b2f2d2100829e70f76a0fc6adabfb
Junius <juniuspun00@gmail.com>
2017-10-18 19:49:21 +0800

    Initial startup
    addition of basic framework
    hand dealing done
    
    to-do:
    figure out how to create online room

---
+++ js/index.js
@@ -0,0 +1,32 @@
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function deal_hand(players,hand,card_arr) {
+	var remade_arr = [];
+	var current_hand;
+	var card_index;
+	for (var i = 0;i < players;i += 1) {
+		current_hand = [];
+		for (var j = 0;j < hand;j += 1) {
+			card_index = getRndInteger(0,card_arr.length);
+			current_hand.push(card_arr[card_index]);
+			card_arr.splice(card_index,1);
+		}
+		remade_arr.push(current_hand);
+	}
+	remade_arr.push(card_arr);
+	return remade_arr;
+}
+$(document).ready(function() {
+	var cards = [
+		[0,0],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[0,8],[0,9],[0,10],[0,11],[0,12],
+		[1,0],[1,1],[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8],[1,9],[1,10],[1,11],[1,12],
+		[2,0],[2,1],[2,2],[2,3],[2,4],[2,5],[2,6],[2,7],[2,8],[2,9],[2,10],[2,11],[2,12],
+		[3,0],[3,1],[3,2],[3,3],[3,4],[3,5],[3,6],[3,7],[3,8],[3,9],[3,10],[3,11],[3,12]
+	];
+	$("#host").click(function() {});
+	$("#join").click(function() {});
+	var game_arr = deal_hand(4,13,cards);
+	
+});
